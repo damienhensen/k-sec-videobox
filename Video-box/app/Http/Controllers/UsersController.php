f41eef5ec73b6e84a281b4a39738eb3cc4e7044a
Blen <44225561+24694Blen@users.noreply.github.com>
2021-01-12 18:05:29 +0100

    update 2

--- Video-box/app/Http/Controllers/UsersController.php
+++ Video-box/app/Http/Controllers/UsersController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\DB;
 class UsersController extends Controller
 {
     public function list(){
-       return 'Bedrijven';
+      $users = DB::table('users')->get();
+      dd($users);
     }
     public function details(){
         
