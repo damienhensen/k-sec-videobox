e3ec58d169c538f11a5248f0f827b0895d51b173
Blen <44225561+24694Blen@users.noreply.github.com>
2021-01-12 15:12:29 +0100

    Post works now

--- Video-box/resources/views/admin/form.blade.php
+++ Video-box/resources/views/admin/form.blade.php
@@ -3,8 +3,8 @@
 @section('content')
 <h1>Product toevoegen</h1>
 
-<form action="{{route('product.store')}" methode="POST">
-
+<form action="{{ route('product.store') }}" method="POST">
+@csrf 
 <div class="form-group">
      <label for="">Title</label>
      <input type="text" class="form-control" name="title"/>
@@ -21,7 +21,7 @@
      <label for="">Date</label>
      <input type="date" class="form-control" name="pub_date"/>
 </div>
-
+<button type="submit" class="btn btn-primary">Opslaan</button>
 
 
 </form>
