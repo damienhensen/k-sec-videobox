a3d7ab871b95e430cd93dff810228c68e72ebfde
Blen <44225561+24694Blen@users.noreply.github.com>
2021-01-12 14:22:41 +0100

    modol

--- Video-box/resources/views/admin/overzicht.blade.php
+++ Video-box/resources/views/admin/overzicht.blade.php
@@ -1,8 +1,33 @@
 @extends('./templates/admin-account')
 
 @section('content')
+<h1>Overzicht</h1>
+  
 
-overzicht
+
+    <table  class="table table-striped">
+        @foreach($reviews as $review)
+            <thead>
+                <tr>
+                    <th scope="col">Title</th>
+                    <th scope="col">Review Content</th>
+                    <th scope="col">Publication</th>
+
+                </tr>
+            </thead>
+
+            <tr>
+
+                <td><a href="{{ route('review.details', ['id'=> $review->id]) }}">{{ $review->title }}</a></td>
+                <td>{{ $review->description }} </td>
+                <td>{{ $review->pub_date }} </td>
 
 
-@endsection
+
+
+            </tr>
+
+        @endforeach
+
+        {{ $reviews->links() }}
+    </table>
